6942af26ebe5cf34a9fa3140028c3e3c2c944b60
asbestboi <ab5562631@gmail.com>
2025-04-06 16:28:38 +0200

    nova class
    verze 4

--- hra.cpp
+++ hra.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h> //https://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
 #define GREEN   "\033[32m"
@@ -10,7 +11,7 @@
 #define MAGENTA "\033[35m"
 #define CYAN    "\033[36m"
 #define WHITE   "\033[37m"
-#include <windows.h>
+
 // Barvicky
 void SetColor(int textColor, int bgColor) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -29,6 +30,8 @@ struct Character {
     bool isBlind;
     bool isVegetarian;
     bool isUndead;
+    bool gamble;
+    bool dodge;
 };
 
 // Funkce pro vytvoreni postavy
@@ -36,17 +39,20 @@ Character chooseClass() {
     int choice;
     Character player;
     while (true) {
-        std::cout << "Vyber si classu:\n1 - Slepec\n2 - Vegetarian\n3 - Kostlivec\n4 - Gambler\n";
+        std::cout << "Vyber si classu:\n1 - Slepec\n2 - Vegetarian\n3 - Kostlivec\n4 - Gambler\n5 - Zlodej\n";
         std::cin >> choice;
 
         if (choice == 1) {
-            player = {"Slepec", 8, 8, 6, 4, 4, 0, true, false, false};
+            player = {"Slepec", 8, 8, 6, 4, 4, 25, true, false, false, false, false};
         } else if (choice == 2) {
-            player = {"Vegetarian", 9, 9, 3, 6, 6, 0, false, true, false};
+            player = {"Vegetarian", 9, 9, 3, 6, 6, 25, false, true, false, false, false};
         } else if (choice == 3) {
-            player = {"Kostlivec", 7, 7, 5, 5, 5, 0, false, false, true};
+            player = {"Kostlivec", 7, 7, 5, 5, 5, 25, false, false, true, false, false};
         } else if (choice == 4) {
-            player = {"Gambler", 6, 6, 3, 5, 5, rand() % 101, false, false, false};
+            player = {"Gambler", 6, 6, 3, 5, 5, 0, false, false, false, true, false};
+        } else if (choice == 5) {
+            player = {"Zlodej", 5, 5, 4, 3, 3, 25, false, false, false, false, true};
+            
         } else {
             std::cout << "Neplatna volba, zkuste znovu.\n";
             continue;
@@ -83,7 +89,9 @@ struct Monster {
 
 // Funkce pro boj s monstry
 void fight(Character &player, Monster monster) {
+    SetColor(0, 4);//cervene
     std::cout << "Pred tebou se zjevil " << (player.isBlind ? "nekdo" : monster.name) << "!\n";
+    SetColor(7, 0);//bila
     while (player.health > 0 && monster.health > 0) {
 
         if (!player.isBlind) {
@@ -140,21 +148,27 @@ void fight(Character &player, Monster monster) {
         }
 
         if (monster.health > 0) {
-            int monsterDamage = rand() % (monster.maxAttack - monster.minAttack + 1) + monster.minAttack;
-            player.health -= monsterDamage;
-            SetColor(4, 0); //cervena
-            std::cout << (player.isBlind ? "nekdo" : monster.name) << " na tebe zautocil za " << monsterDamage << "!\n";
-            SetColor(1, 0); //modra
-            std::cout << "Energie: " << player.energy << "\n";
-            SetColor(10, 0); //zelena
-        std::cout << "Tve zivoty: " << player.health << "\n";
-            SetColor(7, 0); //bila
+            if (player.dodge && rand() % 100 < 30) {
+                SetColor(4, 0); //červená
+                std::cout << (player.isBlind ? "nekdo" : monster.name) << " minul!\n";
+                SetColor(7, 0); //bílá
+            } else {
+                int monsterDamage = rand() % (monster.maxAttack - monster.minAttack + 1) + monster.minAttack;
+                player.health -= monsterDamage;
+                SetColor(4, 0); //červená
+                std::cout << (player.isBlind ? "nekdo" : monster.name) << " na tebe zautocil za " << monsterDamage << "!\n";
+                SetColor(1, 0); //modrá
+                std::cout << "Energie: " << player.energy << "\n";
+                SetColor(10, 0); //zelená
+                std::cout << "Tve zivoty: " << player.health << "\n";
+                SetColor(7, 0); //bílá
+            }
         }
 
         if (player.health <= 0) {
-            SetColor(4, 0); //ted jen cervene :(
+            SetColor(4, 0); //cervena
             std::cout << R"(
-                            ,--.
+                             ,--.
                             {    }
                             K,   }
                            /  ~Y`
@@ -199,8 +213,58 @@ void fight(Character &player, Monster monster) {
     }
     }
 }
+void village(Character &player) {
+    if (player.gamble == true) {
+        player.gold = rand() % 101;
+    }
+    while (true) {
+        SetColor(6, 0); //zluta
+    std::cout << "Zlato: " << player.gold << "\n";
+        SetColor(7, 0); //bila
+        std::cout << "1. Vylepsit zivoty o 5 (15 zlata)\n";
+        std::cout << "2. Vylepsit energii o 5 (20 zlata)\n";
+        std::cout << "3. Vylepsit utok o 2 (20 zlata)\n";
+        std::cout << "4. Odejit z vesnice\n";
+        int choice;
+        std::cin >> choice;
 
+        if (choice == 1) {
+            if (player.gold >= 15) {
+                player.gold -= 15;
+                player.maxHealth += 5;
+                player.health = player.maxHealth;
+                std::cout << "Vylepsil jsi zivoty o 5. Mas " << player.maxHealth << " zivotu.\n";
+            } else {
+                std::cout << "Nemas dostatek zlata na vylepseni zivotu!\n";
+            }
+        } else if (choice == 2) {
+            if (player.gold >= 20) {
+                player.gold -= 20;
+                player.maxEnergy += 5;
+                player.energy = player.maxEnergy;
+                player.energy = std::min(player.maxEnergy, player.energy + 3);
+                std::cout << "Vylepsil jsi energii o 5. Mas " << player.maxEnergy << " energie.\n";
+            } else {
+                std::cout << "Nemas dostatek zlata na vylepseni energie!\n";
+            }
+        } else if (choice == 3) {
+            if (player.gold >= 20) {
+                player.gold -= 20;
+                player.attack += 2;
+                std::cout << "Vylepsil jsi utok o 2. Mas utok: " << player.attack << "\n";
+            } else {
+                std::cout << "Nemas dostatek zlata na vylepseni utoku!\n";
+            }
+        } else if (choice == 4) {
+            std::cout << "Opoustis vesnici.\n";
+            break;
+        } else {
+            std::cout << "Neplatna volba. Zkus to znovu.\n";
+        }
+    }
+}
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
     srand(time(0));
     Character player = chooseClass();
 
@@ -221,8 +285,11 @@ SetColor(10, 0); //zelena
  /___\__,_\__\__,_|\__\___|_\_\
                            )" << '\n';
 SetColor(7, 0); //bila
+std::cout << "nachazis se ve vesnici.\n";
+village(player);
     Monster goblin = {"Goblin", 10 + rand() % 6, 1, player.health/3};
     fight(player, goblin);
-
-    return 0;
+    fight(player, goblin);
+    std::cout << "Ve vesnici sis vylecil vsechny zivoty\n";
+village(player);
 }
