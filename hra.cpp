9bdbded22b7473b9078c14b4c8d73f2242439e50
asbestboi <ab5562631@gmail.com>
2025-05-01 23:04:28 +0200

    pridana knihovna, zlepseni optimalizace

--- hra.cpp
+++ hra.cpp
@@ -6,14 +6,7 @@
 #include <vector>
 #include <functional>
 #include <ctime>
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
+#include "resources/barvy.h"
 void SetColor(int textColor, int bgColor) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, (bgColor << 4) + textColor);
